c02b821e9da10a30b1018dcb01513bca587f51c8
mattyNortz <mentaltoughnesspt@gmail.com>
2020-12-02 06:16:30 +1300

    update

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,9 +1,9 @@
 import javax.swing.JFrame;
 
 public class Pong extends JFrame{
-	static final String WINDOW_TITLE = "Pong";
-	static final int WINDOW_WIDTH = 600;
-	static final int WINDOW_HEIGHT = 800;
+	private static final String WINDOW_TITLE = "Pong";
+	private static final int WINDOW_WIDTH = 600;
+	private final int WINDOW_HEIGHT = 800;
 	
 	public Pong() {
 		setTitle(WINDOW_TITLE);
