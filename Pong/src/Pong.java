4267cf4636608a48c6c41e8b2f7e84f51c599c00
mattyNortz <mentaltoughnesspt@gmail.com>
2020-12-02 05:57:52 +1300

    Update

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,8 +1,23 @@
+import javax.swing.JFrame;
 
-public class Pong {
+public class Pong extends JFrame{
+	static final String WINDOW_TITLE = "Pong";
+	static final int WINDOW_WIDTH = 600;
+	static final int WINDOW_HEIGHT = 800;
+	
+	public Pong() {
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_HEIGHT, WINDOW_WIDTH);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new Pong();
 
 	}
 
