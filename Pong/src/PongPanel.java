d739a1428c1bc9d40a5fc6079f5687d8b224a9f8
mattyNortz <mentaltoughnesspt@gmail.com>
2020-12-02 19:55:57 +1300

    update

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,16 +1,22 @@
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import java.awt.event.KeyListener;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	
-	private static final Color BACKGROUND_COLOR = Color.BLACK;
+	private final static Color BACKGROUND_COLOR = Color.BLACK;
+	private final static int TIMER_DELAY = 5;
 	
 	PongPanel(){
 		setBackground(BACKGROUND_COLOR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+		timer.start();
 	}
 
 	@Override
@@ -34,6 +40,15 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		// TODO Auto-generated method stub
+		update();
+	}
+	
+	private void update() {
+		
+	}
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		
 	}
 
